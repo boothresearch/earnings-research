a5b4a028bea315b4c588afffed40e1217ebc0318
harleenie <harleen.kaur@chicagobooth.edu>
2020-09-15 19:47:26 +0000

    updated code

--- earnings.R
+++ earnings.R
@@ -1,10 +1,13 @@
 # read in csv file
 earnings <- read.csv("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/Earnings/data/earnings.csv")
 
-# save output 
+# save output to text file 
 sink("summary_output.txt", append = FALSE)
 
 # fit regression model to data 
 fitted.model <-  lm(earnings$earn ~ earnings$height)
+# add time to output 
+print(Sys.time(), quote=FALSE)
+# printing summary of regression 
 print(summary(fitted.model))
 closeAllConnections() 
